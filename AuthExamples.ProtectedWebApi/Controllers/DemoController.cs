1fa150074de715953ea00c4a1cfb6f595ec0ea94
Andre Guilherme <andre.guilherme@iatec.com>
2017-10-25 12:03:52 -0200

    Add sample web api

---
+++ AuthExamples.ProtectedWebApi/Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AuthExamples.ProtectedWebApi.Controllers
+{
+    [RoutePrefix("demo")]
+    public class DemoController : ApiController
+    {
+        [HttpGet, Route("test")]
+        public IHttpActionResult Test()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
